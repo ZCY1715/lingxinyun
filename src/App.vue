d01e2a92238c896d48fe4470006e017aa750f13d
wzc <1836912363@qq.com>
2022-06-26 02:54:30 +0800

    结构细分，主页编写完成一部分，文件上传部分的部分编写

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
-<script setup>
+<script >
 import Demo from './components/demo.vue'
+import Sider from './components/Sider/SiderBar.vue'
+
+export default {
+  components: {
+    Sider
+  },
+
+}
 </script>
 
 <template>
-  <Demo/>
+  <Sider></Sider>
+  <el-main>
+    <router-view></router-view>
+  </el-main>
+  
+
 </template>
 
 <style>
+
+/* 定义全局样式 重置样式*/
+html,
+body {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Ubuntu", serif;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+}
+
 
 </style>
