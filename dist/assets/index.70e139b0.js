dacf5eb25555bd4ba6b40c79c146134328c2c5c4
ZCY1715 <zcy1715614103@163.com>
2022-06-26 19:57:21 +0800

    build 部署静态Page

---
+++ dist/assets/index.70e139b0.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index.cd80d142.js";const n={};function o(a,t){return c(),r("div",null,"DiscoverPage")}var _=e(n,[["render",o]]);export{_ as default};
---
+++ dist/assets/index.c982c168.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index.cd80d142.js";const n={};function o(a,t){return c(),r("div",null,"HomePage")}var s=e(n,[["render",o]]);export{s as default};
